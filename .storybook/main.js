7cbe3047a32ddf5811b54d9581fdbe378c9d86cf
Oleh_Cherkashchenko <OC083316@cerner.com>
2023-02-22 01:10:00 +0100

    Add storybook and fix webpack5

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "core": {
+    "builder": {
+      "name": "webpack5"
+    }
+  },
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/vue3"
+}
