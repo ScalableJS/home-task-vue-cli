ea935d64cbd221ca919718b3f4da4fbe2d7dc58f
Oleh_Cherkashchenko <OC083316@cerner.com>
2023-03-01 16:37:17 +0100

    ts, scss

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,14 +4,12 @@ module.exports = {
       "name": "webpack5"
     }
   },
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/preset-scss',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/vue3"
-}
+  framework: '@storybook/vue3',
+};
