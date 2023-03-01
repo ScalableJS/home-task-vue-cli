0f48efa94274bb8827933258948f5098b1499ae8
Oleh_Cherkashchenko <OC083316@cerner.com>
2023-03-01 23:01:12 +0100

    refactoring

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,7 @@
     <LogoItem primary="true" size="small" />
   </header>
   <main>
-    <section>
-      <h2>FIND YOUR MOVIE</h2>
-      <SearchItem
-        primary="true"
-        label="SEARCH"
-        size="large"
-        @submit="handlerSearchSubmit"
-      />
-      <br />
-      <ToggleSwitch
-        primary="true"
-        label="SEARCH BY"
-        labelA="TITLE"
-        labelB="GENRE"
-        v-model="data.isChecked"
-      />
-    </section>
+    <FindMovieSection />
     <ResultsSection />
   </main>
   <footer>
@@ -28,19 +12,7 @@
 </template>
 
 <script setup lang="ts">
-import { defineComponent, reactive } from 'vue';
 import LogoItem from './components/LogoItem.vue';
-import SearchItem from './components/SearchItem.vue';
-import ToggleSwitch from './components/ToggleSwitch.vue';
 import ResultsSection from './components/ResultsSection.vue';
-
-defineComponent({ SearchItem, LogoItem, ToggleSwitch });
-
-const data = reactive({
-  isChecked: false,
-});
-
-const handlerSearchSubmit = function (value: string) {
-  alert(`${value}, ${data.isChecked}`);
-};
+import FindMovieSection from './components/FindMovieSection.vue';
 </script>
---
+++ src/components/MovieDetails.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <div>
+      <img src="" alt="" />
+    </div>
+    <div>
+      <h2></h2>
+    </div>
+  </section>
+</template>
+<script setup lang="ts"></script>
