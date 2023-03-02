a77e21d8e57d458ff499c955d8208bced20fc4dc
Oleh_Cherkashchenko <OC083316@cerner.com>
2023-03-03 00:44:35 +0100

    Add 404 page and routing for search

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './pages/HomePage.vue';
 import MovieDetailsPage from './pages/MovieDetailsPage.vue';
+import NotFoundPage from './pages/NotFoundPage.vue';
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: HomePage },
     {
+      path: '/search',
+      component: HomePage,
+      props: true,
+    },
+    {
       path: '/movie-details/:movieId',
       component: MovieDetailsPage,
       props: true,
+    },
+    {
+      path: '/:catchAll(.*)',
+      component: NotFoundPage,
     },
   ],
 });
